1771d5d040f1b30752dfec48ac65f99eedb3d024
Jason Chi <jason.chi52@gmail.com>
2025-06-05 17:35:41 -0400

    product initialization

---
+++ catalog-service/src/main/java/com/example/catalog_service/model/Product.java
@@ -0,0 +1,28 @@
+package com.example.catalog_service.model;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.math.BigDecimal;
+
+
+/**
+ * Entity class representing a Product in the catalog.
+ * Each product has a name, description, and price.
+ */
+@Entity
+@Data
+public class Product {
+
+    /**
+     * The unique identifier for the product.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String description;
+    
+    //GigDecimal used for better financial calculation
+    private BigDecimal price;
+
+    
+}
